8cb80ae8ddbb658193015a132317bc64a33bc382
Liz P <parisielizabeth@gmail.com>
2019-08-10 03:48:43 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
   
-  def current_user(session_hash)
-    @user = User.find_by_id(session_hash[:user_id])
+  def current_user(session)
+    @user = User.find_by_id(session[:user_id])
   end
   
-  def is_logged_in?(session_hash)
-    !!session_hash[:user_id]
+  def is_logged_in?(session)
+    !!session[:user_id]
   end
 end
